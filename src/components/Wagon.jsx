a8e662989c620608cbd7f8a6ae10144d5e4a0558
Robertcego <lrobertito@gmail.com>
2021-05-20 16:38:52 -0600

    Added styling with @material-ui

--- src/components/Wagon.jsx
+++ src/components/Wagon.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import { connect } from "react-redux";
 import WagonInteraction from "./WagonInteraction";
 
+//? Material ui Components
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
+//!
 function Wagon({ supplies, distance, days, cash }) {
   return (
-    <div>
-      <h1>Wagon Road</h1>
-      <div>
-        <h2>Supplies: {supplies}</h2>
-        <h2>Distance: {distance}</h2>
-        <h2>Days: {days}</h2>
-        <h2>Cash: ${cash}</h2>
-        <WagonInteraction />
-      </div>
-    </div>
+    <Box>
+      <Grid>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <h1>Wagon Road</h1>
+          <h2>Supplies: {supplies}</h2>
+          <h2>Distance: {distance}</h2>
+          <h2>Days: {days}</h2>
+          <h2>Cash: ${cash}</h2>
+          <WagonInteraction />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
--- src/components/WagonInteraction.jsx
+++ src/components/WagonInteraction.jsx
@@ -2,6 +2,22 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { GATHER, TRAVEL, TIPPEDWAGON, SELL, THEFT } from "../actions/names";
 
+//? Material-ui components
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+//? Icons
+import CardTravelIcon from "@material-ui/icons/CardTravel";
+import AddIcon from "@material-ui/icons/Add";
+import CachedIcon from "@material-ui/icons/Cached";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import CloseIcon from "@material-ui/icons/Close";
+
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+
+//!
+
 function WagonInteraction(props) {
   const [day, setDay] = useState(0);
 
@@ -9,21 +25,54 @@ function WagonInteraction(props) {
   //!
   return (
     <div>
-      <button onClick={() => props.dispatch({ type: GATHER })}>Gather</button>
-      <input
-        type="text"
-        name="travel"
-        placeholder="Travel Days"
-        onChange={onChange}
-      />
-      <button onClick={() => props.dispatch({ type: TRAVEL, payload: day })}>
-        Travel
-      </button>
-      <button onClick={() => props.dispatch({ type: TIPPEDWAGON })}>
-        Tipped Wagon
-      </button>
-      <button onClick={() => props.dispatch({ type: SELL })}>Sell</button>
-      <button onClick={() => props.dispatch({ type: THEFT })}>Theft</button>
+      <Container>
+        <TextField
+          maxWidth="md"
+          type="text"
+          name="travel"
+          placeholder="Travel Days"
+          onChange={onChange}
+          autoComplete="false"
+        />
+        <Button
+          startIcon={<CardTravelIcon color="secondary" />}
+          variant="contained"
+          color="primary"
+          size="small"
+          onClick={() => props.dispatch({ type: TRAVEL, payload: day })}
+        >
+          Travel
+        </Button>
+      </Container>
+      <br />
+      <Grid>
+        <ButtonGroup variant="contained" color="secondary" size="small">
+          <Button
+            startIcon={<AddIcon color="primary" size="small" />}
+            onClick={() => props.dispatch({ type: GATHER })}
+          >
+            Gather
+          </Button>
+          <Button
+            startIcon={<CachedIcon color="primary" size="small" />}
+            onClick={() => props.dispatch({ type: TIPPEDWAGON })}
+          >
+            Tipped Wagon
+          </Button>
+          <Button
+            startIcon={<AttachMoneyIcon color="primary" size="small" />}
+            onClick={() => props.dispatch({ type: SELL })}
+          >
+            Sell
+          </Button>
+          <Button
+            startIcon={<CloseIcon color="primary" size="small" />}
+            onClick={() => props.dispatch({ type: THEFT })}
+          >
+            Theft
+          </Button>
+        </ButtonGroup>
+      </Grid>
     </div>
   );
 }
